c710399f745e1aba85529267c23b02e97263dbe7
Jorge Gomez <gomez@chrec.org>
2011-01-20 14:07:18 -0500

    Initial commit, Barebones hw1

---
+++ hw1/hw1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+"""
+File: hw1.py
+Last Modified: Thu Jan 20, 2011 at 14:04
+Author: Jorge Gomez
+
+Description:
+  Will add soon I swear
+"""
+
+import sys
+
+if __name__ == "__main__":
+  main()
